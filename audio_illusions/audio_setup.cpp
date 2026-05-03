543c09e48bdaf1b9e6bc6b978e7b107b0604c33f
Greer Page <greerpage@Greers-MacBook-Pro.local>
2026-05-03 14:19:32 -0400

    commented

--- audio_illusions/audio_setup.cpp
+++ audio_illusions/audio_setup.cpp
@@ -2,26 +2,29 @@
 audio_setup.cpp handles 
 - Initialization of teensy Audio library
 - Volume control
-- 
+- Definition of output devices and master mixer
 */
 
 #include "audio_setup.h"
 
+// volume control pin
 byte VOLUME_PIN = A0;
 
-AudioMixer4 masterMixer;
-AudioOutputI2S audioOut;
-AudioControlSGTL5000 sgtl5000;
+AudioMixer4 masterMixer; // create master mixer for all illusions to pass through
+AudioOutputI2S audioOut; // create single audio output from headphone jack
+AudioControlSGTL5000 sgtl5000; // create audio controller object
 
-AudioConnection masterLeft(masterMixer, 0, audioOut, 0);
-AudioConnection masterRight(masterMixer, 0, audioOut, 1);
+// output the master mix from the headphone jack
+AudioConnection masterLeft(masterMixer, 0, audioOut, 0); // left speacker
+AudioConnection masterRight(masterMixer, 0, audioOut, 1); // right speaker
 
 void setupAudioSystem() {
-  AudioMemory(60);
+  /* set up audio system */
 
-  sgtl5000.enable();
-  sgtl5000.volume(0.5);
+  AudioMemory(60); // allocate memory to audio
+  sgtl5000.enable(); // start comtroller
 
+  // set gain for each channel of master mixer (1 for max gain)
   masterMixer.gain(0, 1.0);
   masterMixer.gain(1, 1.0);
   masterMixer.gain(2, 1.0);
@@ -29,8 +32,9 @@ void setupAudioSystem() {
 }
 
 void volumeControl() {
-  int pot = analogRead(VOLUME_PIN);
-  float volume = pot / 2046.0;
-  if (volume < 0.002) sgtl5000.volume(0);
-  else sgtl5000.volume(volume);
+  /* Control device output volume */
+  int pot = analogRead(VOLUME_PIN); // read volume pot
+  float volume = pot / 2046.0; // keep volume from 0 - 0.5 (comfortable range)
+  if (volume < 0.002) sgtl5000.volume(0); // make sure it goes to 0
+  else sgtl5000.volume(volume); // set volume
 }
