e2af955960e788e18d811ff8c6fc00aa49b091c4
Greer Page <greerpage@Greers-MacBook-Pro.local>
2026-05-03 16:03:18 -0400

    added header file for binaural beats

---
+++ audio_illusions/binaural.h
@@ -0,0 +1,11 @@
+/* Header file for binaural */
+
+#ifndef BINAURAL_H
+#define BINAURAL_H
+
+// functions to use in audio_illusions.ino
+void binauralBeats();
+void stopBinaural();
+void setupBinaural();
+
+#endif
